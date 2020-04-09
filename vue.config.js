d3d5632b7112d348d2852b196c2bc714e81135f5
ZiPeng Ye <zero.cirno@gmail.com>
2020-04-09 11:43:55 +0800

    add 两个开发库: image-webpack-loader, babel-plugin-transform-remove-console

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,30 @@
 /* eslint-disable no-param-reassign, global-require */
 module.exports = {
   publicPath: '.',
+  productionSourceMap: false,
   chainWebpack: (config) => {
+    // 设置 页面标题
     config
       .plugin('html')
       .tap((args) => {
         args[0].title = 'Custom Title';
         return args;
       });
+    // 加载 图片压缩 模块
+    config.module
+      .rule('images')
+      .use('image-webpack-loader')
+      .loader('image-webpack-loader')
+      .options({
+        bypassOnDebug: true,
+      })
+      .end();
   },
   css: {
     loaderOptions: {
       postcss: {
         plugins: [
+          // 加载&配置 px转vw 模块
           require('postcss-px-to-viewport')({
             unitToConvert: 'px',
             viewportWidth: 750,
